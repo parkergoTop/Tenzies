7135a14960132f95d2cfd3cb8faf3f91994c214f
Qingwen Xiao <parkerciao@gmail.com>
2022-09-15 20:24:57 +1000

    after click each element, state changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../style.css"
 import Dice from "./Dice"
+import {nanoid} from "nanoid"
 
 export default function App(){
 
    function allNewDice(){
      const newDice = [];
+   
      for(let i=0; i <10; i++){
-        newDice.push(Math.floor(Math.random()*6+1))
+        newDice.push({value: Math.floor(Math.random()*6+1), isHeld:true, id:nanoid()});
      }
      return newDice;
    
@@ -17,17 +19,34 @@ export default function App(){
    console.log(dices)
 
  
-   
+     /*Update the `rollDice` function to not just roll
+       all new dice, but instead to look through the existing dice
+       to NOT role any that are being `held` */
+    function rollDice(){
+      setDices(preState => preState.map(element => {return element.isHeld? element : {...element, value: Math.floor(Math.random()*6+1) }}));
+    }
     
+    function holdDice(id){
+      setDices(preState => preState.map(element =>  {
+                                             return  element.id ===id?{...element, isHeld: !element.isHeld} : element}))
+    }
+
     return(
          <main className = "main-box">
               <div className="game-box"> 
-              <div className="dice-container">
-                   {dices.map(element =>
-                      {
-                       return <Dice value={element} />
-                      })}
+                   <div className="dice-container">
+                      {dices.map(element =>
+                         {
+                           return <Dice 
+                                       key={element.id}
+                                       id={element.id}
+                                       value={element.value} 
+                                       isHeld={element.isHeld}
+                                       holdDice={() => holdDice(element.id)}/>
+                         })}
                   </div>
+                  <button className="roll-button" onClick={rollDice}>Roll</button >
+
              </div>
 
          </main>)
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,7 +2,11 @@ import React  from "react";
 
 
 export default function Dice(props){
+    const styles ={
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
     return(
-           <h2 className="dice"> {props.value} </h2>
+           <h2 className="dice" style={styles}
+                                onClick={props.holdDice}> {props.value} </h2>
            )
 }
